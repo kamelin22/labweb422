b601f107253b841512d252969762644c6e358f18
kamelin22 <122371666+kamelin22@users.noreply.github.com>
2025-01-15 01:26:42 -0500

    Add vercel.json and update server.js for Vercel deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
 const MoviesDB = require('./modules/moviesDB.js');
+app.use(express.static(__dirname + '/public')); // Serve static files
 
 
 const db = new MoviesDB();
